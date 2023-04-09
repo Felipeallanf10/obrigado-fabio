a9d6e6ae7c538272788d16458b08cc34ab5da5bf
Felipe Allan <felipeallanf10@outlook.com>
2023-04-09 12:49:52 -0300

    adição dos likes, sem animação

--- main.js
+++ main.js
@@ -1,25 +1,272 @@
-/*
+const btn1 = document.querySelector('#b1')
+const btn2 = document.querySelector('#b2')
+const btn3 = document.querySelector('#b3')
+const btn4 = document.querySelector('#b4')
+const btn5 = document.querySelector('#b5')
+const btn6 = document.querySelector('#b6')
+const btn7 = document.querySelector('#b7')
+const btn8 = document.querySelector('#b8')
+const btn9 = document.querySelector('#b9')
+const btn10 = document.querySelector('#b10')
+const btn11 = document.querySelector('#b11')
+const btn12 = document.querySelector('#b12')
+const btn13 = document.querySelector('#b13')
+const btn14 = document.querySelector('#b14')
+const btn15 = document.querySelector('#b15')
+const btn16 = document.querySelector('#b16')
+const btn17 = document.querySelector('#b17')
+const btn18 = document.querySelector('#b18')
 
-var btn = document.getElementsByClassName('.item .botaoo')
-var gostei = document.getElementsByClassName('.gostei')
-var box = document.querySelector('.botao')
+const like1 = document.querySelector('#bb1')
+const like2 = document.querySelector('#bb2')
+const like3 = document.querySelector('#bb3')
+const like4 = document.querySelector('#bb4')
+const like5 = document.querySelector('#bb5')
+const like6 = document.querySelector('#bb6')
+const like7 = document.querySelector('#bb7')
+const like8 = document.querySelector('#bb8')
+const like9 = document.querySelector('#bb9')
+const like10 = document.querySelector('#bb10')
+const like11 = document.querySelector('#bb11')
+const like12 = document.querySelector('#bb12')
+const like13 = document.querySelector('#bb13')
+const like14 = document.querySelector('#bb14')
+const like15 = document.querySelector('#bb15')
+const like16 = document.querySelector('#bb16')
+const like17 = document.querySelector('#bb17')
+const like18 = document.querySelector('#bb18')
 
-btn.addEventListener('click', function () {
-  if (gostei.style.display === 'none') {
-    gostei.style.display = 'flex'
+btn1.addEventListener('click', function () {
+  if (like1.style.display === 'none') {
+    like1.style.display = 'flex'
   } else {
-    gostei.style.display = 'none'
+    like1.style.display = 'none'
   }
 })
 
-function gostar() {
-  box.innerHTML = `
-  <img
-                class="gostei"
-                src="./assets/Logotipo de hamburgueria grunge preto e branco.png"
-                alt=""
-                />
-                `
-              }
-              
-              */
+btn2.addEventListener('click', function () {
+  if (like2.style.display === 'none') {
+    like2.style.display = 'flex'
+  } else {
+    like2.style.display = 'none'
+  }
+})
+
+btn3.addEventListener('click', function () {
+  if (like3.style.display === 'none') {
+    like3.style.display = 'flex'
+  } else {
+    like3.style.display = 'none'
+  }
+})
+
+btn4.addEventListener('click', function () {
+  if (like4.style.display === 'none') {
+    like4.style.display = 'flex'
+  } else {
+    like4.style.display = 'none'
+  }
+})
+btn5.addEventListener('click', function () {
+  if (like5.style.display === 'none') {
+    like5.style.display = 'flex'
+  } else {
+    like5.style.display = 'none'
+  }
+})
+
+btn6.addEventListener('click', function () {
+  if (like6.style.display === 'none') {
+    like6.style.display = 'flex'
+  } else {
+    like6.style.display = 'none'
+  }
+})
+
+btn7.addEventListener('click', function () {
+  if (like7.style.display === 'none') {
+    like7.style.display = 'flex'
+  } else {
+    like7.style.display = 'none'
+  }
+})
+
+btn8.addEventListener('click', function () {
+  if (like8.style.display === 'none') {
+    like8.style.display = 'flex'
+  } else {
+    like8.style.display = 'none'
+  }
+})
+
+btn9.addEventListener('click', function () {
+  if (like9.style.display === 'none') {
+    like9.style.display = 'flex'
+  } else {
+    like9.style.display = 'none'
+  }
+})
+
+btn10.addEventListener('click', function () {
+  if (like10.style.display === 'none') {
+    like10.style.display = 'flex'
+  } else {
+    like10.style.display = 'none'
+  }
+})
+
+btn11.addEventListener('click', function () {
+  if (like11.style.display === 'none') {
+    like11.style.display = 'flex'
+  } else {
+    like11.style.display = 'none'
+  }
+})
+
+btn12.addEventListener('click', function () {
+  if (like12.style.display === 'none') {
+    like12.style.display = 'flex'
+  } else {
+    like12.style.display = 'none'
+  }
+})
+
+btn13.addEventListener('click', function () {
+  if (like13.style.display === 'none') {
+    like13.style.display = 'flex'
+  } else {
+    like13.style.display = 'none'
+  }
+})
+btn14.addEventListener('click', function () {
+  if (like14.style.display === 'none') {
+    like14.style.display = 'flex'
+  } else {
+    like14.style.display = 'none'
+  }
+})
+
+btn15.addEventListener('click', function () {
+  if (like15.style.display === 'none') {
+    like15.style.display = 'flex'
+  } else {
+    like15.style.display = 'none'
+  }
+})
+
+btn16.addEventListener('click', function () {
+  if (like16.style.display === 'none') {
+    like16.style.display = 'flex'
+  } else {
+    like16.style.display = 'none'
+  }
+})
+
+
+btn17.addEventListener('click', function () {
+  if (like17.style.display === 'none') {
+    like17.style.display = 'flex'
+  } else {
+    like17.style.display = 'none'
+  }
+})
+
+btn18.addEventListener('click', function () {
+  if (like18.style.display === 'none') {
+    like18.style.display = 'flex'
+  } else {
+    like18.style.display = 'none'
+  }
+})
+
+/*
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+
+
+function atribuirEvento(id) {
+  document.getElementById(id).addEventListener('click', function () {
+    
+    function atribuirEv(id) {
+      var gostar = document.querySelector(id)
+      if (gostar.style.display === 'none') {
+        gostar.style.display = 'flex'
+      } else {
+        gostar.style.display = 'none'
+      }
+    }
+    
+    var y = document.getElementsByClassName('.gostei')
+    
+    
+    for(i = 0; i < y.length; i++) {
+      var even = y[i].id
+      console.log(even)
+      atribuirEv(even)
+    }
+  })
+}
+//Busca todos os elementos da classe 'btn'
+var x = document.getElementsByClassName('btn')
+
+for (i = 0; i < x.length; i++) {
+  var ev = x[i].id
+  atribuirEvento(ev)
+}
+*/
+/*
+function liked() {
+  var number = 0
+  while (number < 1) {
+    var btn = [
+      btn1,
+      btn2,
+      btn3,
+      btn4,
+      btn5,
+      btn6,
+      btn7,
+      btn8,
+      btn9,
+      btn10,
+      btn11,
+      btn12,
+      btn13,
+      btn14,
+      btn15,
+      btn16,
+      btn17,
+      btn18
+    ]
+    var like = [
+      like1,
+      like2,
+      like3,
+      like4,
+      like5,
+      like6,
+      like7,
+      like8,
+      like9,
+      like10,
+      like11,
+      like12,
+      like13,
+      like14,
+      like15,
+      like16,
+      like17,
+      like18
+    ]
+    print(btn[number])
+    btn[number].addEventListener('click', function () {
+      if (like[number].style.display === 'none') {
+        like[number].style.display = 'flex'
+      } else {
+        like[number].style.display = 'none'
+      }
+    })
+  }
+}
+
+liked()
+*/
